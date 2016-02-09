21a2992c7846b04f25e8cf85e4fb2961cfe1bbc2
Anuradha Vakil <avakil@us.toyota-itc.com>
2016-02-09 13:51:04 -0800

    If statement processing is working now

--- src/matlab/processor/SubstitutionStage.java
+++ src/matlab/processor/SubstitutionStage.java
@@ -19,18 +19,11 @@
  
 package matlab.processor;
 
-import java.lang.reflect.Array;
+
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Stack;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-import com.sun.beans.finder.FieldFinder;
-import com.sun.corba.se.spi.servicecontext.UnknownServiceContext;
-import com.sun.org.apache.xerces.internal.impl.xpath.regex.Match;
 
 import dl.syntax.RealVariable;
 import dl.syntax.Replacement;
@@ -38,8 +31,7 @@ import dl.syntax.Term;
 import dl.syntax.dLFormula;
 import interfaces.text.TextOutput;
 import matlab.syntax.*;
-import sun.org.mozilla.javascript.internal.ast.Assignment;
-import sun.text.normalizer.ReplaceableString;
+
 
 public class SubstitutionStage {
 
@@ -49,11 +41,13 @@ public class SubstitutionStage {
 		// uncomment below to check if-else if-else and more than one assignment statements at the end	
 		//	  MatlabProgram mp = new MatlabProgram (" k1 = 5; k2 = 7*k1; if ( x > k1*6) y = k2*x; elseif (n==0) y = k1*x;else k2 = 100; end z = x^2 + k2*y^2;p=0;");
 		TextOutput.setDebug( true ); TextOutput.useColor( false );
-		MatlabProgram mp = new MatlabProgram (" k1 = 5; k2 = 7*k1; if ( x > k1*6) y = k2*x; else y = k1*x; k2 = 100; end z = x^2 + k2*y^2;p=0;");
+		MatlabProgram mp = new MatlabProgram (" k1 = 5; k2 = 7*k1; if ( x > k1*6) y = k2*x; else y = k1*x; k2 = 100; end p=43; z = x^2 + k2*y^2 + p;");
 		List<RealVariable> inputs = new ArrayList<>();
 		List<RealVariable> outputs = new ArrayList<>();
 		inputs.add( new RealVariable("x"));
-		outputs.add(new RealVariable("y")); outputs.add( new RealVariable ("z") ); outputs.add(new RealVariable("k2"));
+		
+		//outputs.add(new RealVariable("y")); 
+		outputs.add( new RealVariable ("z") );// outputs.add(new RealVariable("k2"));
 		mp = mergePostAssignments( mp );
 		mp = mergePreAssignments( mp, inputs, outputs );
 
@@ -61,7 +55,7 @@ public class SubstitutionStage {
 	}
 	//TODO: all terms in form of inputs and outputs; simplify the data structures
 	public static MatlabProgram mergePostAssignments( MatlabProgram mp ) {
-		List<Statement> matlabStatements = mp.getStatements();
+	//	List<Statement> matlabStatements = mp.getStatements();
 		
 		Stack<Statement> postAssignments = new Stack<>();
 		while ( !(mp.peekLastStatement() instanceof IfStatement ) ) {
@@ -71,6 +65,8 @@ public class SubstitutionStage {
 		while ( !(postAssignments.isEmpty() )) {
 			postAssignmentList.add(postAssignments.pop());
 		}
+		
+			
 		IfStatement ifStatement = ((IfStatement)mp.consumeLastStatement());
 		for ( MatlabProgram ifCase : ifStatement.getPrograms() ) {
 			ifCase.append(postAssignmentList);
@@ -87,26 +83,31 @@ public class SubstitutionStage {
 														List<RealVariable> inputs,
 														List<RealVariable> outputs ){
 		
-		while( newMatlabProgram.peekFirstStatement() instanceof AssignmentStatement ) {
+		int index = 0;
+		while( newMatlabProgram.getStatements().get(index) instanceof AssignmentStatement ) {
 			AssignmentStatement assignment = (AssignmentStatement) newMatlabProgram.peekFirstStatement();
 			
-			//if ( !outputs.contains( assignment.getLHS() ) ) {
-			if ( true ) {
+			if ( outputs.contains( assignment.getLHS() ) ) {
+				index++;	
+			} else {
 				newMatlabProgram.consumeFirstStatement();
 			}
 			
 		//	TextOutput.debug("MatlabProgram: "+ newMatlabProgram.getStatements().toString()+" Assignment: "+assignment );
-			newMatlabProgram = replaceWithAssignment( newMatlabProgram, assignment);
+			Replacement replacement = new Replacement( assignment.getLHS(), assignment.getRHS() );
+			newMatlabProgram = replaceWithAssignment( newMatlabProgram, replacement, outputs);
 		}
 		//TextOutput.info(newMatlabProgram.toString());
 		return newMatlabProgram;
 
 	}
 
-	public static MatlabProgram replaceWithAssignment( MatlabProgram program, AssignmentStatement assignment ) {
+	public static MatlabProgram replaceWithAssignment( MatlabProgram program, 
+														Replacement replacement,
+														List<RealVariable> outputs ) {
 		
 		AssignmentStatement sReplaced;
-		Replacement replacement = new Replacement((RealVariable)assignment.getLHS(), assignment.getRHS());
+		//Replacement replacement = new Replacement((RealVariable)assignment.getLHS(), assignment.getRHS());
 
 		MatlabProgram result = new MatlabProgram();
 		for ( Statement s : program.getStatements() ) {
@@ -124,7 +125,7 @@ public class SubstitutionStage {
 
 				for ( MatlabProgram thisProgram : programs ) {
 					//	TextOutput.debug("This program is: " + thisProgram.toString() );
-					newPrograms.add( replaceWithAssignment( thisProgram, assignment ) );
+					newPrograms.add( replaceWithAssignment( thisProgram, replacement, outputs ) );
 				}
 
 				result.append( new IfStatement( newConditions, newPrograms ) );
@@ -132,13 +133,13 @@ public class SubstitutionStage {
 			} else if ( s instanceof AssignmentStatement ) {
 				RealVariable sLHS = (RealVariable)((AssignmentStatement)s).getLHS();
 				Term sRHS = ((AssignmentStatement)s).getRHS();
-				if ( sLHS.equals( assignment.getLHS() )) {
-					sReplaced = new AssignmentStatement( sLHS , sRHS.replace( replacement ) );
-					replacement = new Replacement( sLHS, sRHS.replace(replacement) );
-				} else {
-					sReplaced = new AssignmentStatement( sLHS , sRHS.replace( replacement ) );
+				
+				replacement.put( sLHS, sRHS.replace(replacement) );
+				sReplaced = new AssignmentStatement( sLHS , sRHS.replace( replacement ) );		
+				
+				if ( outputs.contains(sLHS) ) {
+					result.append( sReplaced );
 				}
-				result.append( sReplaced );
 			}
 
 		}
