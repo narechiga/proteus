f53e1c1ff00dd8c787f6ac603a1db232a56fd983
Nikos Arechiga <narechiga@us.toyota-itc.com>
2016-02-10 13:22:02 -0800

    working on translationtools

---
+++ src/translationtools/matlab2ha/Matlab2HybridAutomaton.java
@@ -0,0 +1,11 @@
+package translationtools.matlab2ha;
+
+import ha.*;
+import matlab.syntax.*;
+
+public class Matlab2HybridAutomaton {
+
+//	public static HybridAutomaton convert( MatlabProgram program ) {
+//
+//	}
+}
