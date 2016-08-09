ec3c604d43beb00ed5d3829d3a8ce0e43c2bc585
Nikos Arechiga <narechiga@us.toyota-itc.com>
2016-08-08 18:05:42 -0700

    fixed broken-ness with a temporary hack

--- src/dl/dL.java
+++ src/dl/dL.java
@@ -3,8 +3,8 @@ package dl;
 import dl.syntax.*;
 
 public class dL {
-	public static dLFormula simplify( dLFormula ) {
-		
+	public static dLFormula simplify( dLFormula formula ) {
+		return null;
 	}
 
 }
