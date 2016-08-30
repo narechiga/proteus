9b715611d50b1431264e329be3a8ec4ea39f1814
Sumanth Dathathri <sdathathri@us.toyota-itc.com>
2016-08-29 17:30:18 -0700

    Parallelized clustersample

--- src/dl/logicsolvers/abstractions/LogicSolverInterface.java
+++ src/dl/logicsolvers/abstractions/LogicSolverInterface.java
@@ -9,6 +9,12 @@ package dl.logicsolvers.abstractions;
 
 import interfaces.text.*;
 import java.util.*;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.List;
 //import java.io.*;
 
 import dl.semantics.*;
@@ -70,24 +76,90 @@ public abstract class LogicSolverInterface {
 		return multiSample( formulas, numSamples, resolution );
 
 	}
-	public ArrayList<Valuation> clusterSample(dLFormula formula, int numSamples, ArrayList<Double> radii ) throws Exception {
-		
+	
+	public ArrayList<Valuation> clusterSample(dLFormula formula, int numSamples, ArrayList<Double> radii,int parallelize_flag ) throws Exception {
+
+
 		ArrayList<Valuation> points = multiSample(formula,numSamples,radii.get(0));
-		for (int i=1;i<radii.size();i++)
-		{
-			points=clusterSampleBase(formula,points,numSamples,radii.get(i),radii.get(i-1));
-		}
+			for (int i=1;i<radii.size();i++)
+			{
+				points=clusterSampleBase(formula,points,numSamples,radii.get(i),radii.get(i-1),parallelize_flag);
+			}
+		
+		
 		return points;
 	}
 	
-	protected ArrayList<Valuation> clusterSampleBase(dLFormula formula, ArrayList<Valuation> center_points,int numSamples, double SR, double BR) throws Exception{
-		ArrayList<Valuation> points= new ArrayList<Valuation>();
-		for (int i=0;i<center_points.size();i++)
-		{
-			points.addAll(multiSample(new AndFormula(formula,createBallExclusionFormula(center_points.get(i),new Real(BR)).negate()),numSamples,SR));
+	/*
+	 * else{
+			ExecutorService executor=Executors.newFixedThreadPool(points.size());
+			Callable<Integer> task = new Callable() {
+					public int run()
+					{
+				    try {
+				        TimeUnit.SECONDS.sleep(1);
+				        return 123;
+				    }
+				    catch (InterruptedException e) {
+				        throw new IllegalStateException("task interrupted", e);
+				    }
+				}
+
+					@Override
+					public Integer call() throws Exception {
+						// TODO Auto-generated method stub
+						return 123;
+					}
+			
+			};
+			Future<Integer> future = executor.submit(task);
+			
+			
 		}
-		return points;
-	}
+	 */
+	protected ArrayList<Valuation> clusterSampleBase(final dLFormula formula, final ArrayList<Valuation> center_points,final int numSamples, final double SR, final double BR,int parallelize_flag) throws Exception{
+			ArrayList<Future<ArrayList<Valuation>>> futures = new ArrayList<Future<ArrayList<Valuation>>>();
+			ArrayList<Valuation> points= new ArrayList<Valuation>();
+			ExecutorService executor=Executors.newFixedThreadPool(1);
+
+			if(parallelize_flag==1){
+
+				executor=Executors.newFixedThreadPool(center_points.size());
+				}
+				else
+				{
+				executor=Executors.newFixedThreadPool(1);
+
+				}
+			for (int i=0;i<center_points.size();i++)
+			{	final int j=i;
+				
+				Callable<ArrayList<Valuation>> task = new Callable<ArrayList<Valuation>>() {
+						public ArrayList<Valuation> call() throws Exception {
+							// TODO Auto-generated method stub
+	
+							    try {
+							    	
+							        ArrayList<Valuation> newpoints=(multiSample(new AndFormula(formula,createBallExclusionFormula(center_points.get(j),new Real(BR)).negate()),numSamples,SR));
+							        return newpoints;
+							    }
+							    catch (InterruptedException e) {
+							        throw new IllegalStateException("task interrupted", e);
+							    }						
+						}
+				
+				};
+				Future<ArrayList<Valuation>> future = executor.submit(task);
+				futures.add(future);
+			}
+			
+			for (Future<ArrayList<Valuation>> future: futures)
+			{
+				points.addAll(future.get());
+			}
+			return points;
+		}
+	
 
 	public ArrayList<Valuation> multiSample( List<dLFormula> thisSet, int numberOfPoints, double suggestedRadius ) {
                 ArrayList<dLFormula> queryFormulas = new ArrayList<dLFormula>();
