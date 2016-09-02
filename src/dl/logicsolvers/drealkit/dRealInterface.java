11db774a954ea3eb21e4eae39758b9698249e991
Sumanth <sdathathri@us.toyota-itc.com>
2016-09-02 11:02:27 -0700

    Added timeouts to clusterSample

--- src/dl/logicsolvers/drealkit/dRealInterface.java
+++ src/dl/logicsolvers/drealkit/dRealInterface.java
@@ -24,18 +24,6 @@ import dl.parser.PrettyPrinter;
 
 public class dRealInterface extends LogicSolverInterface {
 
-	// COLORS! OMG COLORS!
-	public final String ANSI_RESET = "\u001B[0m";
-	public final String ANSI_BLACK = "\u001B[30m";
-	public final String ANSI_RED = "\u001B[31m";
-	public final String ANSI_GREEN = "\u001B[32m";
-	public final String ANSI_YELLOW = "\u001B[33m";
-	public final String ANSI_BLUE = "\u001B[34m";
-	public final String ANSI_PURPLE = "\u001B[35m";
-	public final String ANSI_CYAN = "\u001B[36m";
-	public final String ANSI_WHITE = "\u001B[37m";
-	public final String ANSI_BOLD = "\u001B[1m";
-
 	public double precision = 0.00001;
 	public boolean debug = false;
 
